79b3e3a9633a61435f2045d0d52fea5a1c1fee0a
rindi <rindi@mail.sfsu.edu>
2014-11-12 09:16:29 -0800

    Attempting reinstall of devise in light of db issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   resources :player_profiles
 
   resources :game_boards
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#home'
-
+  #root to: "home#index"
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
