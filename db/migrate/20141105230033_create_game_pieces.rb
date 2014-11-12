76a9f47b28a15ffdda8073a5042a7566867266ca
Jordan Guinn <jordan_guinn@hotmail.com>
2014-11-12 13:23:41 -0800

    Fixing STI functionality

--- db/migrate/20141105230033_create_game_pieces.rb
+++ db/migrate/20141105230033_create_game_pieces.rb
@@ -1,7 +1,7 @@
 class CreateGamePieces < ActiveRecord::Migration
   def change
     create_table :game_pieces do |t|
-      t.string :type
+      t.string :piecetype
       t.integer :x
       t.integer :y
       t.integer :type
