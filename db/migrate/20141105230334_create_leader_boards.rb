f9cd01b361121d0973b284efd5ccae6d34f8c69a
Frank Dye <otherwhitefrank@gmail.com>
2014-11-12 14:36:36 -0800

    Fixed DB:Migrate problem with baseline

--- db/migrate/20141105230334_create_leader_boards.rb
+++
@@ -1,8 +0,0 @@
-class CreateLeaderBoards < ActiveRecord::Migration
-  def change
-    create_table :leader_boards do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20141105231444_create_game_boards.rb
+++
@@ -1,8 +0,0 @@
-class CreateGameBoards < ActiveRecord::Migration
-  def change
-    create_table :game_boards do |t|
-
-      t.timestamps
-    end
-  end
-end
