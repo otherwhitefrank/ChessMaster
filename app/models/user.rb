daf816d64557953c2526a05ba965f8c3695beb6f
Frank Dye <otherwhitefrank@gmail.com>
2014-11-30 15:06:04 -0800

    Pushing changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,24 @@ class User < ActiveRecord::Base
 
   has_one :player
 
-  attr_accessor :updated_at
+  attr_accessor :updated_at, :playing_game
+
+  after_initialize do
+    init_player
+    update_time
+    puts "User has been initialized!"
+  end
+
+  def init_player
+    puts "Creatng new Player object"
+    new_player = Player.create
+    new_player.user = self
+  end
+
+  def update_time
+    "Updated updated_at time"
+    self[:updated_at] = Time.now
+  end
 
   def online?
     updated_at > 10.minutes.ago
