2fad1ecc7caee0cf14ed89867f4d05f796de0f02
Frank Dye <otherwhitefrank@gmail.com>
2014-12-03 14:19:31 -0800

    Fixed online users to online show users that are not yourself

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,29 +1,37 @@
 class WelcomeController < ApplicationController
+  attr_accessor :player_1, :player_2
+
   def start_game
     #Start a game with the next active player
-    player_1 = current_user
+    @player_1 = current_user
 
     #Find players that are active and not playing
-
-
-
   end
 
   def home
     #base welcome page
+    @users        = User.all
+    @online_users = []
+    @current_user_email = current_user['email']
+
+    @users.each do |user|
+      @online_now = user['updated_at'] > 10.minutes.ago
+      @user_name  = user['email']
+      if (@user_name != @current_user_email)
+        @online_users.push(user)
+      end
+    end
   end
+
 
   def start_game_with_player
     #Start a game with a specific player
-    player_1 = current_user
-    player_2 = params[:second_player]
-
+    @player_1 = current_user
+    @player_2 = params[:second_player]
     #Start game
   end
 
   def continue_game
     #Restart a previously saved game
   end
-
-
 end
